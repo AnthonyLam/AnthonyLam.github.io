d01429ae1198c8cbb47752080a6d6b2436717fa8
AnthonyLam <thelamacmdr@gmail.com>
2015-03-03 01:11:40 -0800

    No favicon for now

---
+++ _posts/post.py
@@ -0,0 +1,27 @@
+import datetime
+import sys
+import string
+
+def filenameConvert(name):
+    name = name.lower()
+    name = name.replace(' ','_')
+    return name
+
+now = datetime.datetime.now()
+dateString = now.strftime('%y-%m-%d') 
+
+if len(sys.argv) > 0:
+    print('Defaulting to standard name')
+    title = 'Hello World'
+else:
+    title = sys.argv(1)
+    print('The title is: ',title)
+# Open file
+fo = open(dateString + '-' + filenameConvert(title) + ".markdown","w")
+fo.write("---\n")
+fo.write("layout: post\n")
+fo.write("title: " + title +"\n")
+fo.write("date: " + dateString  + "\n")
+fo.write("categories: ")
+fo.write("---\n")
+fo.close()
