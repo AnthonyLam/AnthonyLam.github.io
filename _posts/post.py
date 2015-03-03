2d4fdb264dd92d8eba1b5507c516f4d77081c8e3
AnthonyLam <thelamacmdr@gmail.com>
2015-03-03 01:35:53 -0800

    Python script for creating a new post
    Usage: python post.py "Title"

--- _posts/post.py
+++ _posts/post.py
@@ -4,24 +4,24 @@ import string
 
 def filenameConvert(name):
     name = name.lower()
-    name = name.replace(' ','_')
+    name = name.replace(' ','-')
     return name
 
 now = datetime.datetime.now()
 dateString = now.strftime('%y-%m-%d') 
 
-if len(sys.argv) > 0:
+if len(sys.argv) == 0:
     print('Defaulting to standard name')
     title = 'Hello World'
 else:
-    title = sys.argv(1)
+    title = sys.argv[1]
     print('The title is: ',title)
 # Open file
-fo = open(dateString + '-' + filenameConvert(title) + ".markdown","w")
+fo = open(dateString + '-' + filenameConvert(title) + ".md","w")
 fo.write("---\n")
 fo.write("layout: post\n")
 fo.write("title: " + title +"\n")
 fo.write("date: " + dateString  + "\n")
-fo.write("categories: ")
+fo.write("categories: \n")
 fo.write("---\n")
 fo.close()
